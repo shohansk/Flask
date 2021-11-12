bde9c28350fee41e73a240a129db24301368ca49
shohansk <shohankhansk111@gmail.com>
2021-11-12 18:34:45 +0600

    major change

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     first_name = "Shohan"
-    tittle = "this is cool"
+    tittle = "This is cool thing"
     stuff = "this is <strong>Bold</strong> "
     pizza = ['Pepparoni','cheese','moyda',41]
     return render_template("index.html",
@@ -19,3 +19,13 @@ def home():
 @app.route("/user/<name>")
 def user(name):
     return render_template("user.html",user_name = name)
+# Create custom Error pages
+#invalid URL
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html") ,404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html") ,500
